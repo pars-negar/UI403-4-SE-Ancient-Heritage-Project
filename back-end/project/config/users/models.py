828ff9c648dab40b8206260f1926fd75f8045892
Sara Raoufi <sararaoufi.r@gmail.com>
2025-03-09 21:47:24 +0330

    remove backend.gitkeep

--- back-end/project/config/users/models.py
+++ back-end/project/config/users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+#در این قسمت دو نوع کاربر سیستم را مشخص کرده ایم
+class Users(AbstractUser):
+    Role_Choise=(
+        ('tour_manager', 'مسئول تور')
+        ('regular_user', 'کاربر عادی')
+    )
 
 # Create your models here.
