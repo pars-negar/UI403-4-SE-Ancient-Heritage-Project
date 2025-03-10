4138a20b78fb207f167e5f8c0e382e557f700261
Sara Raoufi <sararaoufi.r@gmail.com>
2025-03-10 10:16:38 +0330

    delete some thing wrong

--- back-end/project/config/config/settings.py
+++ back-end/project/config/config/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'tours',
     'reserve',
     'message',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
--- back-end/project/config/users/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import projectusers
-
-admin.site.register(projectusers)
-# Register your models here.
-
