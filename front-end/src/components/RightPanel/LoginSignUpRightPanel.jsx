202cb631ef9a73a8b3dcd2cf85c60c97f3a42448
yasamanRafiee2004 <yasamanrafiee67@gmail.com>
2025-04-04 12:36:50 +0330

    add log in sign up page

--- front-end/src/components/RightPanel/LoginSignUpRightPanel.jsx
+++ front-end/src/components/RightPanel/LoginSignUpRightPanel.jsx
@@ -1,15 +1,18 @@
-
 import React from "react";
-import styles from'./login-sign-up-right-panel.module.css';
+import styles from "./login-sign-up-right-panel.module.css";
+
 
 
 const LoginSignUpRightPanel = () => {
+  const backgroundImage = "/assets/images/left-panel-background.jpg";
+  const iranMap = './assets/icons/logo.svg';
   return (
-    <div className={styles.rightPanel}>
-      <div className={styles.backgroundImage}></div>
-      <img src="./assets/icons/logo.svg" alt="پرچم ایران" className={styles.centerImage} />
+    <div className={styles.loginSignUpRightPanel} style={{ backgroundImage: `url(${backgroundImage})` }}>
+      <img src={iranMap} alt="Iran Map" className={styles.iranMapLoginSignUp} />
     </div>
   );
 };
 
 export default LoginSignUpRightPanel;
+
+
