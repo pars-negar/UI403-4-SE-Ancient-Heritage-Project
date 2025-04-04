a60d1536f29439a4ae81203859d125966bfa9425
yasamanRafiee2004 <yasamanrafiee67@gmail.com>
2025-04-04 18:32:39 +0330

    add loginsignup page

--- front-end/src/components/LeftPanel/LoginSignUpLeftPanel.jsx
+++ front-end/src/components/LeftPanel/LoginSignUpLeftPanel.jsx
@@ -6,11 +6,11 @@ const LoginSignupLeftPanel = () => {
     <div className={styles.LSLPanel}>
       <div className={styles.LSLPanelContent}>
         <h2 className={styles.welcomeText}>به سامانه پارس نگار خوش آمدید</h2>
-        <img className={styles.sloganImage} src="./assets/images/text.png" alt="به تاریخ سفر کنید و شکوه ایران را لمس کنید!" />
+        <img className={styles.LSLImage} src="./assets/images/text.png" alt="به تاریخ سفر کنید و شکوه ایران را لمس کنید!" />
         <div className={styles.buttonsContainer}>
-          <button className={styles.button}>ورود به حساب کاربری</button>
-          <button className={styles.button}>ثبت‌نام کاربر</button>
-          <button className={styles.button}>ثبت‌نام مسئول تور</button>
+          <button className={styles.darkBlue}>ورود به حساب کاربری</button>
+          <button className={styles.midblue}>ثبت‌نام کاربر</button>
+          <button className={styles.lightblue}>ثبت‌نام مسئول تور</button>
         </div>
       </div>
     </div>
