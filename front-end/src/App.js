ae3b7f26328e036ed5b64bca7ba2eadbc4dad73e
aminiinazanin <amininazanin0382@gmail.com>
2025-04-10 13:45:11 +0430

    edit

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -10,6 +10,7 @@ import TourismAttractionCard from "./components/card/tourismAttractionCard.jsx";
 import HomePage from "./pages/Home/Home.jsx";
 import "./App.css";
 import TourPage from "./pages/TourPage/TourPage.jsx";
+import Home from "./pages/Home/Home.jsx";
 
 
 
@@ -24,7 +25,7 @@ function App() {
       {/* <Route exact path="/" element={ <VerificationPage /> }/> */}
       {/* <Route exact path="/" element={ <PasswordRecovery/> }></Route> */}
       {/* <Route path="/tour/:id" element={<TourPage />} /></Routes> */}
-      </Routes>
+      <Route path="/" element={<Home />} /></Routes>
     </BrowserRouter>
   );
 }
