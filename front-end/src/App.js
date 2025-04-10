28647eb83be154d129279c9cc0765c8ebbbe7f12
yasamanRafiee2004 <yasamanrafiee67@gmail.com>
2025-04-10 13:31:23 +0330

    add tourAttraction component

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,6 +6,7 @@ import UserSignUpPage from "./pages/UserSignUp/UserSignUp.jsx";
 import PasswordRecovery from "./pages/PasswordRecovery/PasswordRecovery";
 import LoginPage from "./pages/LoginPage/LoginPage.jsx"
 import LoginSignUp from "./pages/LoginSignUp/LoginSignUp.jsx"
+import TourismAttractionCard from "./components/card/tourismAttractionCard.jsx";
 
 
 
@@ -15,10 +16,12 @@ function App() {
     <Routes>
       {/* <Route exact path="/" element={ <UserSignUpPage/> }></Route> */}
       {/* <Route exact path="/" element={ <LoginPage/> }></Route> */}
-      <Route exact path="/" element={ <UserSignUpPage/> }></Route>
+      {/* <Route exact path="/" element={ <UserSignUpPage/> }></Route> */}
       {/* <Route exact path="/" element={ <PasswordRecovery/> }></Route> */}
       {/* <Route exact path="/" element={ <LoginSignUp/> }></Route> */}
       {/* <Route exact path="/" element={ <VerificationPage /> }/> */}
+
+
       </Routes>
     </BrowserRouter>
   );
