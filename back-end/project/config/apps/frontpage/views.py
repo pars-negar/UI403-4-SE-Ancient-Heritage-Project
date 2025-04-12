8779043889ed4a95dc55c2d8c8679bae7abb0a9d
Fateme Mahdizade <f.mahdizade20@gmail.com>
2025-04-12 17:58:06 +0430

    Add home page api

--- back-end/project/config/apps/frontpage/views.py
+++ back-end/project/config/apps/frontpage/views.py
@@ -1,45 +1,32 @@
-# from django.shortcuts import render
-# from rest_framework.views import APIView
-# from rest_framework.response import Response
-# from rest_framework import status
+from django.shortcuts import render
 
-# from .serializers import AttractionSerializer, TourSerializer, FAQSerializer
-# from apps.tour.models import Attraction,Tour
-# from apps.faq.models import FAQ
+# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializer import HeaderImageSerializer , AttractionSerializer, TourSerializer, FAQSerializer # 👈 این لازمه
 
-# class FrontPageAPIView(APIView):
-#     def get(self, request):
-#         attractions = Attraction.objects.all()[:3]
-#         tours = Tour.objects.order_by('-start_date')[:3]
-#         faqs = FAQ.objects.all()[:3]
 
-#         data = {
-#             'attractions': AttractionSerializer(attractions, many=True).data,
-#             'tours': TourSerializer(tours, many=True).data,
-#             'faqs': FAQSerializer(faqs, many=True).data,
-#         }
-#         return Response(data, status=status.HTTP_200_OK)
+from .serializer import AttractionSerializer, TourSerializer, FAQSerializer
+from apps.tour.models import Attraction
+from apps.tour.models import Tour
+from apps.faq.models import FAQ
+from .models import HeaderImage
+ 
 
-# # Create your views here.
-# from rest_framework.views import APIView
-# from rest_framework.response import Response
-# from rest_framework import status
 
-# from .serializers import AttractionSerializer, TourSerializer, FAQSerializer
-# from tour.models import Attraction
-# from tours.models import Tour
-# from faq.models import FAQ
+class HomePageAPIView(APIView):
+    def get(self, request):
+        attractions = Attraction.objects.all()[:3]
+        tours = Tour.objects.order_by('-start_date')[:3]
+        faqs = FAQ.objects.all()[:3]
+        headers = HeaderImage.objects.filter(show_on_homepage=True)[:5]  
 
-# class FrontPageAPIView(APIView):
-#     def get(self, request):
-#         # فیلتر: مثلا ۳ جاذبه برتر، ۳ تور جدید، ۳ سوال متداول
-#         attractions = Attraction.objects.all()[:3]
-#         tours = Tour.objects.order_by('-start_date')[:3]
-#         faqs = FAQ.objects.all()[:3]
-
-#         data = {
-#             'attractions': AttractionSerializer(attractions, many=True).data,
-#             'tours': TourSerializer(tours, many=True).data,
-#             'faqs': FAQSerializer(faqs, many=True).data,
-#         }
-#         return Response(data, status=status.HTTP_200_OK)
+        data = {
+            'attractions': AttractionSerializer(attractions, many=True).data,
+            'tours': TourSerializer(tours, many=True).data,
+            'faqs': FAQSerializer(faqs, many=True).data,
+            'headers': HeaderImageSerializer(headers, many=True).data,  # 👈 اضافه شد
+        }
+        return Response(data, status=status.HTTP_200_OK)
+print
