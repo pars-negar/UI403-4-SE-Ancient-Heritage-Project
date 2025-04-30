d62607cc0d2fdbccafc8ba08579c533ecd9d587b
yasamanRafiee2004 <yasamanrafiee67@gmail.com>
2025-04-30 13:51:56 +0330

    turn the project into a tailwind project

---
+++ front-end/tailwind.config.mjs
@@ -0,0 +1,8 @@
+export default {
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+    // Ensure Tailwind purges unused CSS in production automatically
+  }
