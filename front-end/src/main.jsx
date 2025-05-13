18828a9333a31a41115178180113802c55362fd5
Zeinab-Jannati <Zaynab5@gmail.com>
2025-05-13 13:35:23 +0330

    upload front code from old repo

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -50,10 +50,6 @@ createRoot(document.getElementById('root')).render(
 
         <Route exact path="/LoginSignUp" element={ <LoginSignUp/> }></Route>
         <Route exact path="/LoginSignUp/UserSignUp" element={ <UserSignUpPage/> }></Route>
-        <Route exact path="/TourleaderSignUp" element={ <TourleaderSignUp/> }></Route>
-        <Route exact path="/login" element={ <LoginPage/> }></Route>
-        <Route exact path="/passwordRecovery" element={ <PasswordRecovery/> }></Route>
-        <Route path="/verifyotp" element={< VerificationPage/>} />
         <Route exact path="/LoginSignUp/TourleaderSignUp" element={ <TourleaderSignUp/> }></Route>
         <Route exact path="/LoginSignUp/login" element={ <LoginPage/> }></Route>
         <Route exact path="/passwordRecovery" element={ <PasswordRecovery/> }></Route>
@@ -66,7 +62,6 @@ createRoot(document.getElementById('root')).render(
         {/* <Route exact path="/" element={<TourismAttractionCard image="./assets/images/takht-jamshid.png" title="تخت جمشید" description="!شکوه بی‌همتای امپراتوری هخامنشی را از نزدیک لمس کنید" backgroundColor="#FF8C1A"/>}></Route> */}
         {/* <Route exact path="/" element={ < FourCityCards/>}></Route> */}
         <Route exact path="/setnewpass" element={ < SetNewPassword/>}></Route>
-
 
      
       </Routes>
