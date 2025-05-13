a1f16926a6bb5947718f7ce0716a183028b73f92
Sara Raoufi <sararaoufi.r@gmail.com>
2025-05-13 20:56:22 +0330

    add api for tour page

--- back-end/project/config/apps/frontpage/urls.py
+++ back-end/project/config/apps/frontpage/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomePageAPIView
+from .views import HomePageAPIView ,TourPageAPIView
 
 urlpatterns = [
     path('', HomePageAPIView.as_view(), name='homepage'),
+     path('tour-page/', TourPageAPIView.as_view(), name='tour-page'),
 ]
