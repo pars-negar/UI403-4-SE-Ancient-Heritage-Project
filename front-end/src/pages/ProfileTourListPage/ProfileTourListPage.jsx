48e19b32607bea8481e5f8bbf7155c5939985054
yasamanRafiee2004 <yasamanrafiee67@gmail.com>
2025-05-30 00:30:29 +0330

    change export

--- front-end/src/pages/ProfileTourListPage/ProfileTourListPage.jsx
+++ front-end/src/pages/ProfileTourListPage/ProfileTourListPage.jsx
@@ -1,7 +1,7 @@
 const ProfileTourListPage = () => {
     return (
         <div className="text-right mr-3.5">
-        <hr className="bg-[var(--color-gray)] h-0.2 border-none w-full" />
+        <hr className="border !border-[var(--color-gray)] opacity-100 w-full h-[0.1rem] m-0"/>
         <h1 className="text-xl font-bold mb-6 mr-1 border-r-4 border-[#205781] pr-1.5 ">لیست تورها</h1>
         </div>
 
