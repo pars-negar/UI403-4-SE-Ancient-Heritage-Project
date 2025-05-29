297831f9abf98df55d257a7dbfc080d651e876ab
Sara Raoufi <sararaoufi.r@gmail.com>
2025-05-29 19:14:41 +0330

    some change

---
+++ back-end/list_structure.py
@@ -0,0 +1,13 @@
+import os
+
+def list_files(start_path):
+    with open("structure.txt", "w", encoding="utf-8") as f:
+        for root, dirs, files in os.walk(start_path):
+            level = root.replace(start_path, '').count(os.sep)
+            indent = ' ' * 4 * level
+            f.write(f"{indent}{os.path.basename(root)}/\n")
+            subindent = ' ' * 4 * (level + 1)
+            for file in files:
+                f.write(f"{subindent}{file}\n")
+
+list_files(".")
