ecdcab1a00eebadc3c53bcee288bac1774c6f856
yasamanRafiee2004 <yasamanrafiee67@gmail.com>
2025-06-06 16:54:03 +0330

    edit tourismcard

--- front-end/src/components/card/tourismAttractionCard.jsx
+++ front-end/src/components/card/tourismAttractionCard.jsx
@@ -1,15 +1,22 @@
-
 import styles from './tourism-attraction-card.module.css';
 
 const TourismAttractionCard = ({ image, title, description, backgroundColor = '#FF9800' }) => {
   return (
     <div className={styles.cardWrapper}>
-      <div className={styles.imageWrapper}>
-        <img src={image} alt={title} className={styles.cardImage} />
-      </div>
-      <div className={styles.cardContent} style={{ backgroundColor: backgroundColor}}>
+      <div
+        className={styles.imageWrapper}
+        style={{
+          backgroundImage: `url(${image})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+        }}
+      />
+      <div className={styles.cardContent} style={{ backgroundColor: backgroundColor }}>
         <h2 className={styles.cardTitle}>{title}</h2>
-        <p className={styles.cardDescription} style={{ fontFamily: 'Vazirmatn', fontWeight: 500}}>{description}</p>
+        <p className={styles.cardDescription} style={{ fontFamily: 'Vazirmatn', fontWeight: 500 }}>
+          {description}
+        </p>
       </div>
     </div>
   );
