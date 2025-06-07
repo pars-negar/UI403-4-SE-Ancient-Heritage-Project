542631f0b00230f0e92cbd30a8a57030ec3b82a1
Fz Fldgr <fzfolad83@gmail.com>
2025-06-08 01:21:10 +0330

    Fix login control

--- back-end/project/config/apps/frontpage/urls.py
+++ back-end/project/config/apps/frontpage/urls.py
@@ -2,25 +2,27 @@
 from django.urls import path
 from .views import (HomePageAPIView ,TourPageAPIView, AttractionDetailAPIView,
                     TourDetailView,AttractionPageAPIView,get_cities_with_places,
-                    TourDetailAPIView, TourListAPIView, TourUpdateAPIView, TourSoftDeleteAPIView,get_origins_and_destinations,CreateTourAPIView
+                    TourDetailAPIView, TourListAPIView, TourUpdateAPIView,
+                    TourSoftDeleteAPIView,get_origins_and_destinations,CreateTourAPIView,
+                    UserProfileView, DashboardRedirectAPIView
 )
 urlpatterns = [
     path('', HomePageAPIView.as_view(), name='homepage'),
-     path('tour-page', TourPageAPIView.as_view(), name='tour-page'),
-     path('tour/<int:pk>/', TourDetailView.as_view(), name='tour-detail'),
+    path('tour-page', TourPageAPIView.as_view(), name='tour-page'),
+    path('tour/<int:pk>/', TourDetailView.as_view(), name='tour-detail'),
     path('attraction/<int:pk>/', AttractionDetailAPIView.as_view(), name='attraction-detail'),
-     path('attraction-page/', AttractionPageAPIView.as_view(), name='attraction-page'),
+    path('attraction-page/', AttractionPageAPIView.as_view(), name='attraction-page'),
     path('places/cities/', get_cities_with_places),
     path('dashboard/tours/', TourListAPIView.as_view(), name='tour-list'),
     path('dashboard/tours/<int:pk>/', TourDetailAPIView.as_view(), name='tour-detail'),
     path('dashboard/tours/<int:pk>/edit/', TourUpdateAPIView.as_view(), name='tour-update'),
-
+    path('profile/', UserProfileView.as_view(), name='user-profile'),
     path('dashboard/my-tours/delete/', TourSoftDeleteAPIView.as_view(), name='delete-my-tour'),
- path('tour/cities/', get_origins_and_destinations),
-
-
+    path('tour/cities/', get_origins_and_destinations),
     path('dashboard/my-tours/delete/', TourSoftDeleteAPIView.as_view(), name='delete-my-tour'), 
     path('dashboard/tours/create/', CreateTourAPIView.as_view(), name='create-tour'),
+    path('dashboard-url/', DashboardRedirectAPIView.as_view(), name='dashboard-url'),
+
 
 ]
 
