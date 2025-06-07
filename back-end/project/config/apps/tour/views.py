9796e16c09439c5ab8c9be25c41ff678f7bf3c5e
Fz Fldgr <fzfolad83@gmail.com>
2025-06-07 09:29:23 +0330

    Fix tour & reserve

--- back-end/project/config/apps/tour/views.py
+++ back-end/project/config/apps/tour/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .serializers import TourFilterSerializer, TourSerializer, TourDetailSerializer , AttractionSerializer, TourFilterSerializer, TourSerializer,TourCreateSerializer
+from .serializers import TourFilterSerializer, TourSerializer , AttractionSerializer, TourFilterSerializer,TourCreateSerializer, TourDetailSerializer
 from .models import Attraction
 from rest_framework import viewsets
 from rest_framework.response import Response
