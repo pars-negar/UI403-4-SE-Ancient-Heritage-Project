e973b048e10e60ec0d644ff75a6fa81d5ec1014c
Zeinab-Jannati <Zaynab5@gmail.com>
2025-06-07 07:48:35 +0330

    fix tourtab

---
+++ front-end/src/components/TourTab/TourTab.jsx
@@ -0,0 +1,60 @@
+import { useState, useEffect, use } from "react";
+import axios from 'axios';
+
+import Tabs from "../../components/Tabs/Tabs";
+import TourCard from "../../components/Card/TourCard";
+
+const TourTab = () => {
+    const [tours, setTours] = useState([]);
+    const [loading, setLoading] = useState(true);
+    
+
+    useEffect(() => {
+    getData();
+    }, []);
+    
+
+    const getData = async () => {
+        try {
+            const response = await axios.get(
+                "https://parsnegarback.liara.run/api/homepage"
+            );
+            console.log(response)
+          if (response && response.status === 200) {
+            console.log(response.data);
+            setTours(response.data.tours || []);
+            setLoading(false);
+          } else {
+            console.error("Failed to fetch data", response);
+            setLoading(false);
+          }
+        } catch (error) {
+          console.error("Error during fetch: ", error);
+          setLoading(false);
+        }
+      };
+
+
+    return ( 
+        <Tabs labels={['تورهای اخیر' ,'تورهای برتر']} className="!mt-[10rem] w-full">
+          <div id="home-tour-card">
+            <div style={{ display: 'flex', gap: '8px', justifyContent: 'center', flexWrap: 'wrap' ,marginTop:'90px' }}>
+              {loading ? (
+                <p>در حال بارگذاری...</p>
+              ) : (
+                tours && tours.map((tour) => (
+                    <TourCard key={tour.id} tour={tour} />
+                    
+                )) 
+              )}
+            </div>
+        </div>
+
+          <div>
+            
+          </div>
+        </Tabs>
+     );
+}
+ 
+export default TourTab;
