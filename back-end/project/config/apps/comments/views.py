82a19ea6b7376d55a60cbd259ff4db6b8ae10a5b
Sara Raoufi <sararaoufi.r@gmail.com>
2025-06-07 07:47:24 +0330

    .

--- back-end/project/config/apps/comments/views.py
+++ back-end/project/config/apps/comments/views.py
@@ -1,8 +1,7 @@
 from rest_framework import viewsets, permissions
-from .models import SiteComment
-from .serializers import SiteCommentSerializer
-from apps.users.permissions import *
-from rest_framework.permissions import *
+from .models import SiteComment, TourComment
+from .serializers import SiteCommentSerializer, TourCommentSerializer
+from apps.users.permissions import IsOwnerOrAdmin, IsOwnerOrReadOnly 
 
 class SiteCommentViewSet(viewsets.ModelViewSet):
     queryset = SiteComment.objects.all().order_by('-created_at')
@@ -13,4 +12,22 @@ class SiteCommentViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
 
     def get_queryset(self):
-        return SiteComment.objects.all()
+        user = self.request.user
+        if user.is_authenticated and user.is_staff:
+            return SiteComment.objects.all().order_by('-created_at')
+        return SiteComment.objects.filter(is_approved=True).order_by('-created_at')
+
+
+class TourCommentViewSet(viewsets.ModelViewSet):
+    queryset = TourComment.objects.all().order_by('-created_at')
+    serializer_class = TourCommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_authenticated and user.is_staff:
+            return TourComment.objects.all().order_by('-created_at')
+        return TourComment.objects.filter(is_approved=True).order_by('-created_at')
