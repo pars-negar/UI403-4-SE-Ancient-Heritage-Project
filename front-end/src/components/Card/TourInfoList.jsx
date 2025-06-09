7eda66fcf4df87e1eeef97e3be08c1c32a794682
Sara Raoufi <sararaoufi.r@gmail.com>
2025-06-09 23:49:23 +0330

    connect tout list for tourmanager

--- front-end/src/components/Card/TourInfoList.jsx
+++ front-end/src/components/Card/TourInfoList.jsx
@@ -1,39 +1,24 @@
+import React from "react";
 import TourInfoCard from "./TourInfoCard";
 
-import pasargadImage from "../../assets/images/pasargad-tour.png";
-import zigooratImage from "../../assets/images/zigoorat-tour.png";
-import golestanImage from "../../assets/images/golestan-tour.png";
+const TourInfoList = ({ tours }) => {
+  if (!tours || tours.length === 0) {
+    return <p className="text-center mt-4" style={{ fontFamily: 'Vazirmatn' }}>هیچ توری یافت نشد.</p>;
+  }
 
-const tours = [
-  {
-    title: "تور تخت جمشید و پاسارگاد",
-    destination: "استان فارس (شیراز، مرودشت)",
-    duration: "3 روز و 2 شب",
-    price: "3,000,000",
-    imageUrl: pasargadImage,
-  },
-  {
-    title: "تور شوش و زیگورات چغازنبیل",
-    destination: "خوزستان (شوش، هفت‌تپه، چغازنبیل)",
-    duration: "2 روز و 1 شب",
-    price: "2,500,000",
-    imageUrl: zigooratImage,
-  },
-  {
-    title: "تور کاخ گلستان و بازار تاریخی تهران",
-    destination: "تهران",
-    duration: "1 روزه",
-    price: "900,000",
-    imageUrl: golestanImage,
-  },
-];
-
-const TourInfoList = () => {
   return (
     <div className="p-8 text-right">
-      <div className="flex flex-col content-center items-center mt-5">{tours.map((tour, index) => (
-        <TourInfoCard key={index} {...tour} />
-      ))}
+      <div className="flex flex-col content-center items-center mt-5">
+        {tours.map((tour, index) => (
+          <TourInfoCard
+            key={tour.id || index}
+            title={tour.tour_name}
+            destination={tour.destination}
+            duration={tour.duration}
+            price={tour.price}
+            imageUrl={tour.image_url}
+          />
+        ))}
       </div>
     </div>
   );
